077bbeb044f6146e25d265af0a26711e2e403898
Luke Karrys <luke@lukekarrys.com>
2021-11-26 15:38:22 -0700

    reset MSYSTEM before test

--- test/lib/utils/is-windows-bash.js
+++ test/lib/utils/is-windows-bash.js
@@ -1,8 +1,11 @@
 const t = require('tap')
 const mockGlobal = require('../../fixtures/mock-globals.js')
 
-const isWindowsBash = () =>
-  t.mock('../../../lib/utils/is-windows-bash.js')
+const isWindowsBash = () => {
+  delete require.cache[require.resolve('../../../lib/utils/is-windows-bash.js')]
+  delete require.cache[require.resolve('../../../lib/utils/is-windows.js')]
+  return require('../../../lib/utils/is-windows-bash.js')
+}
 
 t.test('posix', (t) => {
   mockGlobal(t, { 'process.platform': 'posix' })
@@ -14,7 +17,7 @@ t.test('posix', (t) => {
 t.test('win32', (t) => {
   mockGlobal(t, { 'process.platform': 'win32' })
 
-  mockGlobal(t, { 'process.env.TERM': 'dumb' })
+  mockGlobal(t, { 'process.env.TERM': { TERM: 'dumb', MSYSTEM: undefined } })
   t.equal(isWindowsBash(), false, 'false when not mingw or cygwin')
 
   mockGlobal(t, { 'process.env.TERM': 'cygwin' })
